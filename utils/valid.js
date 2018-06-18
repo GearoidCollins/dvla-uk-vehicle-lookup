1a3b02b8131bbbb2b0e0f359cf23a81f66330a52
Gearoid Collins <gearoidcollins@me.com>
2018-06-19 00:59:49 +0100

    exceed the daily rate limit error

--- utils/valid.js
+++ utils/valid.js
@@ -5,6 +5,9 @@ module.exports = function valid({ data }) {
   if (/no\svehicle/i.test(data)) {
     throw new Error('Invalid Vehicle registration');
   }
+  if (/exceeded\sthe\slimit/i.test(data)) {
+    throw new Error('Exceeded the request limit - reg lookups');
+  }
   if (/restricted/i.test(data)) {
     throw new Error('Third-party request limit reached');
   }
